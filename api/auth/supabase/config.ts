89922d04bd4b0a0acf6aa61d60d29855bbfad985
wolfsidevstudios <emartinezra112@icloud.com>
2025-12-30 18:39:03 -0800

    Add api/auth/supabase/config.ts via GitZip

---
+++ api/auth/supabase/config.ts
@@ -0,0 +1,7 @@
+
+export const CONFIG = {
+  clientId: 'b4802cb8-1f8b-4c4c-81a1-e6774bcfbec7',
+  clientSecret: 'sba_08a9e9d43ad75f3433b60c9f8e3edcd0060496ef',
+  tokenUrl: 'https://api.supabase.com/v1/oauth/token',
+  authUrl: 'https://api.supabase.com/v1/oauth/authorize'
+};
